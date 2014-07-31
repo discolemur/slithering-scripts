60137810ba811bfef94f0678414faa207f9620d0
discolemur <nickthejensen@gmail.com>
2014-07-31 14:13:20 -0600

    Changed conversion things.

--- fastaToPfam.py
+++ fastaToPfam.py
@@ -1,14 +1,19 @@
 #! /bin/env python
 
 import sys
-from Bio import SeqIO
+from Bio import AlignIO
 
 def main(args) :
 	if (len(args) != 3) :
 		print "Gimme an input and output file next time.\n"
 		print "Usage: %s <input.fasta> <output.msf>\n" %args[0]
 		return 1
-	SeqIO.convert(args[1], "fasta", args[2], "stockholm")
+	input = open(args[1], 'r')
+	output = open(args[2], 'w')
+	alns = AlignIO.parse(input, 'fasta')
+	AlignIO.write(alns, output, 'stockholm')
+	output.close()
+	input.close()
 
 if __name__ == "__main__" :
 	main(sys.argv)
