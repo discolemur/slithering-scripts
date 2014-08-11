3a40c5f56569289b8a6e18f54312a3ed8cfdee5b
discolemur <nickthejensen@gmail.com>
2014-08-11 15:48:54 -0600

    Added a way to import helper functions. Added a way to get "complete clusters" from hamstr.

---
+++ helpers.py
@@ -0,0 +1,16 @@
+#! /bin/env python
+
+## This function performs an action on a list of things and prints the progress as it goes.
+# Arguments:
+# list === a bunch of items
+# action === a function to perform on the items (the first argument in the function must be an item in the list)
+# *args === optional arguments to pass into the action function
+def do_progress_update(list, action, *args) :
+	total = len(list)
+	checkpoint = total / 10
+	counter = 0
+	for item in list :
+		if (counter % checkpoint == 0) :
+			print('Progress: %d/%d (%.2f%%)' %(counter, total, (counter * 100.0 / total)))
+		action(item, *args)
+
--- multihamstr/align_clusters_hamstr.py
+++ multihamstr/align_clusters_hamstr.py
@@ -7,7 +7,7 @@ import subprocess
 
 
 def info() :
-	print 'This program takes a directory of clusters (output from multihamstr.py)'
+	print 'This program must be run inside a directory of clusters (output from multihamstr.py)'
 	print 'and puts the clusters (conserved or semi) into their own directory, aligned.'
 
 def is_semiconserved(file, num_sp) :
@@ -38,6 +38,19 @@ def is_conserved(file, num_sp) :
 		result = False
 	return result
 
+def has_all_species(file, num_sp) :
+	file_handle = open(file, 'r')
+	species = set()
+	result = True
+	for line in file_handle :
+		if line[0] == '>' :
+			sp = line.split('|')[2]
+			species.add(sp)
+	file_handle.close()
+	if len(species) != num_sp :
+		result = False
+	return result
+
 def is_valid(file, num_sp, conserve) :
 	if conserve :
 		return is_conserved(file, num_sp)
@@ -62,14 +75,33 @@ def align_files(files, num_sp, conserve) :
 			new_name = file.split('.')[0] + '.aln'
 			subprocess.call("mafft %s > %s/%s" %(file, dir, new_name), stdout=nowhere, stderr=subprocess.STDOUT, shell=True)
 
+def complete_clusters(files, num_sp) :
+	nowhere = open(os.devnull, 'w')
+	dir = 'complete_clusters'
+	total = len(files)
+	counter = 0
+	percent = total / 10
+	if not os.path.exists(dir) :
+		os.makedirs(dir)
+	for file in files :
+		counter += 1
+		if counter % percent == 0 :
+			print "Progress: (%d/%d) %.2f%%" %(counter, total, (1.0 * counter)/total)
+		if has_all_species(file, num_sp) :
+			new_name = file.split('.')[0] + '.aln'
+			subprocess.call("mafft %s > %s/%s" %(file, dir, new_name), stdout=nowhere, stderr=subprocess.STDOUT, shell=True)
+
 def main(args) :
 	info()
-	if len(args) != 3 or ( args[1] != '-c' and args[1] != '-s' ) :
-		print 'Usage: %s [-c (conserved) or -s (semiconserved)] <number of organisms>' %args[0]
+	if len(args) != 3 or ( args[1] != '-c' and args[1] != '-s' and args[1] != '-a') :
+		print 'Usage: %s [-c (conserved) or -s (semiconserved) or -a (contains all species, allowing paralogs)] <number of organisms>' %args[0]
 		return 1
 	files = glob.glob('*.fa')
-	conserve = ( args[1] == '-c' )
-	align_files(files, int(args[2]), conserve)
+	if (args[1] == '-a') :
+		complete_clusters(files, int(args[2]))
+	else :
+		conserve = ( args[1] == '-c' )
+		align_files(files, int(args[2]), conserve)
 	return 0
 
 if __name__ == '__main__' :
